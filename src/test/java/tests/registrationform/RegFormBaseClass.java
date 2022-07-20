a5b539371cfef3f2eb90293f7f295084b02291c6
Ivan L <fallouttoday@mail.ru>
2022-07-21 02:58:33 +0300

    added demoQAtest remote running

--- src/test/java/tests/registrationform/RegFormBaseClass.java
+++ src/test/java/tests/registrationform/RegFormBaseClass.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import tests.registrationform.pages.RegistrationFormPage;
 
 public class RegFormBaseClass {
@@ -14,8 +15,15 @@ public class RegFormBaseClass {
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         System.out.println("Конфигурация теста установлена");
     }
 
