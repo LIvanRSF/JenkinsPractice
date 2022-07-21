c96317e25a2b981a064c6cd7cf28d63665b46f30
Ivan L <fallouttoday@mail.ru>
2022-07-21 03:16:42 +0300

    added attachments

--- src/test/java/tests/registrationform/RegFormBaseClass.java
+++ src/test/java/tests/registrationform/RegFormBaseClass.java
@@ -3,9 +3,11 @@ package tests.registrationform;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import tests.registrationform.attachments.Attach;
 import tests.registrationform.pages.RegistrationFormPage;
 
 public class RegFormBaseClass {
@@ -32,5 +34,13 @@ public class RegFormBaseClass {
         testData = new RegFormTestData();
         formPage.openPage();
         System.out.println("Тестовые данные сгенерированы. Веб-страница открыта");
+    }
+
+    @AfterEach
+    void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
