b2ddd96f393750d3128250a833016778df61ab55
Ivan L <fallouttoday@mail.ru>
2022-07-29 22:35:54 +0300

    test with owner

--- src/test/java/tests/registrationform/RegFormBaseClass.java
+++ src/test/java/tests/registrationform/RegFormBaseClass.java
@@ -2,7 +2,9 @@ package tests.registrationform;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +13,7 @@ import tests.registrationform.attachments.Attach;
 import tests.registrationform.pages.RegistrationFormPage;
 
 public class RegFormBaseClass {
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
     RegistrationFormPage formPage = new RegistrationFormPage();
     RegFormTestData testData;
 
@@ -23,9 +26,15 @@ public class RegFormBaseClass {
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        String remoteWebDriverUrl = System.getProperty("remoteDriverUrl", "selenoid.autotests.cloud/wd/hub");
+
+        Configuration.remote =
+            String.format("https://%s:%s@%s", config.login(), config.password(), remoteWebDriverUrl);
         System.out.println("Конфигурация теста установлена");
     }
 
